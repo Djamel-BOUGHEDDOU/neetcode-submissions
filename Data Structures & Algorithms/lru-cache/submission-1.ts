43c7872d83fa3d0de8180954797e4502a2a9357e
Djamel BOUGHEDDOU <131941125+Djamel-BOUGHEDDOU@users.noreply.github.com>
2026-08-05 12:19:05 +0100

    Add: lru-cache - submission-1

---
+++ Data Structures & Algorithms/lru-cache/submission-1.ts
@@ -0,0 +1,50 @@
+class LRUCache {
+    /**
+     * @param {number} capacity
+     */
+    capacity: number;
+    cache: Map<number, number>;
+
+    constructor(capacity: number) {
+        this.capacity = capacity;
+        this.cache = new Map();
+    }
+
+    /**
+     * @param {number} key
+     * @return {number}
+     */
+    get(key: number): number {
+        if (!this.cache.has(key)) return -1;
+
+        // we need to set this as recently used:
+
+        const res = this.cache.get(key);
+
+        // this is done to preserve the priority
+        this.cache.delete(key);
+        this.cache.set(key, res);
+
+        return res;
+    }
+
+    /**
+     * @param {number} key
+     * @param {number} value
+     * @return {void}
+     */
+    put(key: number, value: number): void {
+        if (!this.cache.has(key)) {
+            // this mean we will create a new pair of key-value
+            // we must check the capacity
+            if (this.cache.size == this.capacity) {
+                const leastRecentlyUsedKey = this.cache.keys().next().value;
+                this.cache.delete(leastRecentlyUsedKey);
+            }
+            this.cache.set(key, value);
+        } else {
+            this.cache.delete(key);
+            this.cache.set(key, value);
+        }
+    }
+}
